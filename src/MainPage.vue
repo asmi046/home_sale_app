9673a12469aa7b0cc417da744bfd68ace1bbf4cc
asmi046 <asmi046@gmail.com>
2023-02-07 20:48:22 +0300

    tw-разметка

--- src/MainPage.vue
+++ src/MainPage.vue
@@ -1,5 +1,18 @@
 <template>
-  <h1 class="text-3xl font-bold underline" >Main</h1>
+  <header class="flex border-gray-300 text-white text-2xl flex-col bg-sblue font-header">
+    <div class="container mx-auto px-4 py-4 flex justify-between">
+      <div class="logo m-auto ml-0 flex">
+        <p class="">Подбор квартир</p>
+      </div>
+      
+      <a class="menu bg-white w-9 h-9 rounded-full my-auto bg-no-repeat bg-center" :style="{'background-image': 'url(' + require('./assets/icons/logo-blue.svg') + ')'}" href="#"></a>
+    </div>
+    <div class="hdecor w-full h-5 bg-white rounded-t-xl "></div>
+  </header>
+
+  <main class="h-10">
+
+  </main>
 </template>
 
 <script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'sred': '#C10010',
+        'sblue': '#003C8C'
+      },
+
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+        'header': ['"Exo 2"']
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addBase, theme }) {
+      addBase({
+        'body': { backgroundColor: theme('white') },
+      })
+    })
+  ],
 }
